3c831ed775cf3ba96b21028dc9952feaa7d3abe0
Sébastien Miro (Plein Sud) <94846599+PSIT-smiro@users.noreply.github.com>
2023-06-26 17:50:04 +0200

    Fixed broken AppHeader.js

--- src/components/layout/AppHeader.js
+++ src/components/layout/AppHeader.js
@@ -16,7 +16,6 @@ import { AppHeaderDropdown, AppHeaderSearch } from 'src/components/header'
 import { TenantSelector } from '../utilities'
 import cyberdrainlogolight from 'src/assets/images/CIPP.png'
 import cyberdrainlogodark from 'src/assets/images/CIPP_Dark.png'
-import pleinsudlogo from 'src/assets/images/psgroupe-logo.svg'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretSquareLeft, faCaretSquareRight } from '@fortawesome/free-solid-svg-icons'
@@ -70,13 +69,7 @@ const AppHeader = () => {
             <AppHeaderDropdown />
           </CNavItem>
           <CNavItem>
-          <CImage
-            className="me-2"
-            src={
-              pleinsudlogo
-            }
-            height={40}
-          />  
+          <CImage className="me-2" src="src/assets/images/psgroupe-logo.svg" height={40}/>  
           </CNavItem>
         </CHeaderNav>
       </CHeader>
